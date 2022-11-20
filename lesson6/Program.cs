70d2de6fa91e51fba04b07085b81e0af5ba591c4
Knyazev Vyacheslav <ks38@mail.ru>
2022-11-20 11:16:49 +0300

    added folder6

---
+++ lesson6/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if (b > max) {
+    max = b;
+} else if (c > max) {
+    max = c;
+} else if (d > max) {
+    max = d;
+} else max = e;
+
+Console.WriteLine("max = " + max);
