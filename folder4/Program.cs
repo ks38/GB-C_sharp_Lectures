1ecd753d96bd4763b7ad5a9c07f194e7411d5e77
Knyazev Vyacheslav <ks38@mail.ru>
2022-11-20 11:11:08 +0300

    added folder5

--- folder4/Program.cs
+++ folder4/Program.cs
@@ -1,4 +1,6 @@
-﻿double numA = 11;
-double numB = 5;
+﻿double numA = new Random().Next(1,100);
+double numB = new Random().Next(1,100);
 
-Console.WriteLine(numA / numB);
+Console.WriteLine("numA = " + numA);
+Console.WriteLine("numB = " + numB);
+Console.WriteLine("sum = " + numA / numB);
---
+++ folder5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter username");
+string userName = Console.ReadLine();
+
+if (userName.ToLower() == "masha") {
+Console.WriteLine("Hello, " + userName);
+} else if (userName.ToLower() == "vasia") {
+Console.WriteLine("Hello, " + userName);
+} else Console.WriteLine("You're not human");
