b8aaf905f689913951f309058b9202e2a2a010f1
Vyacheslav Knyazev - PC <ks38@mail.ru>
2022-11-29 21:27:36 +0300

    lessons new

---
+++ Lesson2/folder2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 11, 45, 55, 14, 987, 13, 54 };
+
+int n = array.Length;
+
+int find = 55;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ Lesson2/folder3/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] argument)
+{
+    int length = argument.Length;
+    int i = 0;
+    while (i < length)
+    {
+        argument[i] = new Random().Next(1, 10);
+        i++;
+    }
+}
+
+void PrintArray(int[] argum)
+{
+    int count = argum.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(argum[position] + " ");
+        position++;
+    }
+}
+
+int IndexOf(int[] argu, int find)
+{
+    int position = -1;
+    for (int i = 0; i < argu.Length; i++)
+    {
+        if (argu[i] == find)
+        {
+            position = i;
+            break;
+        }
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine("\n");
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
